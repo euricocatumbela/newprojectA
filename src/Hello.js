d8dd784a4966bd3a9c1d20f1b87c31d5469eae41
Aerobotics <artificialinteligence@Aeroboticss-MacBook-Air.local>
2020-09-30 21:12:34 +0200

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Location from "../src/Location";
+function Hello() {
+  return (
+    <div>
+      <h1 className="ClassA">Hello World</h1>
+      <Location />
+      <Location />
+    </div>
+  );
+}
+
+export default Hello;
---
+++ src/Location.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Location() {
+  return <p className="">Welcome to Cape Town</p>;
+}
+
+export default Location;
